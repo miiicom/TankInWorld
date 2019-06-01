5c58bfe2a885f725f820b366e53f84588fc85c16
miiicom <miiicom1230@gmail.com>
2019-05-31 19:18:38 -0600

    Bug solved. Need to use GetWorld->GetDeltaTime()

--- TankInWorld/Source/TankInWorld/TankBarrel.cpp
+++ TankInWorld/Source/TankInWorld/TankBarrel.cpp
@@ -5,8 +5,8 @@
 
 void UTankBarrel::Elevate(float RelativeDegree)
 {
-	auto time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("relativedegree is  %f"), RelativeDegree);
+	auto time = GetWorld()->GetDeltaSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("time is  %f"), time);
 	float ElevationChange = FMath::Clamp<float>(RelativeDegree,-1.0f,1.0f) * MaxDegreePersecond * time;
 	//float ElevationChange = RelativeDegree * MaxDegreePersecond * time;
 	float clampedElevationChange = RelativeRotation.Pitch+ ElevationChange;
